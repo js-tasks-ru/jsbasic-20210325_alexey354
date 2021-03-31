0c1766548995a3b35792b190f12483953cf82b35
wp-live <zalkigames@gmail.com>
2021-03-31 16:09:10 +0300

    lesson 2

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,6 @@
 function checkSpam(str) {
-  // ваш код...
+  let denied = ['1xBet', 'XXX'];
+  let result = denied.filter(word => str.toLowerCase().search(word.toLowerCase()) >= 0 );
+
+  return (result.length) ? true : false;
 }
